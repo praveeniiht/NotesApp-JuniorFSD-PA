3e4c1c74170d96fe9ea1b6298a5af60965346dbe
Praveen Kumar <gs_praveen@rediffmail.com>
2020-06-16 13:47:45 +0530

    Some files are deleted

--- NotesApp/src/test/java/com/examples/projects/controller/HomeControllerTest.java
+++ NotesApp/src/test/java/com/examples/projects/controller/HomeControllerTest.java
@@ -1,14 +1,28 @@
 package com.examples.projects.controller;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.apache.commons.io.FileUtils;
 import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import com.examples.projects.service.NotesService;
 import com.examples.projects.service.NotesServiceImpl;
 
@@ -16,49 +30,59 @@ import com.examples.projects.service.NotesServiceImpl;
 class HomeControllerTest {
 
 	
-	//NotesService notesService = new NotesServiceImpl();
+	@Mock
+	private NotesService notesService;
 	
-	@Autowired
+	@InjectMocks
+	private HomeController homeController;
+	
 	private MockMvc mockmvc;
 	
-//	@InjectMocks
-//	private HomeController homeController;
-	
-	
-	@Test
-	public void shouldReturnDefaultMessage() throws Exception {
-				
+	static {
+		File file = new File("output_controller_revised.txt");
+		if (file.exists()) {
+			try {
+				FileUtils.forceDelete(new File("output_controller_revised.txt"));
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				// e.printStackTrace();
+			}
+		}
 	}
-	
-	@Test
-	void testShowHomePage() {
 		
-		fail("Not yet implemented");
+	@BeforeEach
+	public void setup() {
+		MockitoAnnotations.initMocks(this);
+		this.mockmvc = MockMvcBuilders.standaloneSetup(homeController).build();
 	}
-
+	
 	@Test
-	void testAddNote() {
-		fail("Not yet implemented");
+	void testShowHomePage() throws Exception {
+	MvcResult result = this.mockmvc.perform(get("/")).andExpect(view().name("index")).andReturn();
+	File file = new File("output_controller_revised.txt"); 
+	FileUtils.write(file, "\n testShowHomePage ="+(result.getResponse().getStatus() == HttpStatus.OK.value() ? true : false), true);
 	}
 
+	
 	@Test
-	void testDeleteNote() {
-		fail("Not yet implemented");
+	void testStatusPage() throws Exception {
+		MvcResult result = this.mockmvc.perform(get("/statusManager")).andExpect(view().name("statusmanager")).andReturn();
+		File file = new File("output_controller_revised.txt"); 
+		FileUtils.write(file, "\n testStatusPage ="+(result!=null ? true : false), true);
 	}
-
+	
 	@Test
-	void testEditNote() {
-		fail("Not yet implemented");
+	void testViewAllPage() throws Exception {
+		MvcResult result = this.mockmvc.perform(get("/viewlAll")).andExpect(view().name("viewall")).andReturn();
+		File file = new File("output_controller_revised.txt"); 
+		FileUtils.write(file, "\n testViewAllPage ="+(result!=null ? true : false), true);
 	}
-
+	
 	@Test
-	void testStatusManager() {
-		fail("Not yet implemented");
+	void testUpdatePage() throws Exception {
+		MvcResult result = this.mockmvc.perform(get("/updateNote")).andExpect(view().name("index")).andReturn();
+		File file = new File("output_controller_revised.txt"); 
+		FileUtils.write(file, "\n testUpdatePage ="+(result!=null ? true : false), true);
 	}
-
-	@Test
-	void testViewlAll() {
-		fail("Not yet implemented");
-	}
-
+	
 }
